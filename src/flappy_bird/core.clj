5250f9b21b7cdc41bc82a7df9523a9689c714d46
Logan Campbell <colin@logaan.net>
2014-02-05 19:58:09 +1100

    base cljs

---
+++ src/flappy_bird/core.clj
@@ -0,0 +1,6 @@
+(ns flappy-bird.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/flappy_bird/core_test.clj
@@ -0,0 +1,7 @@
+(ns flappy-bird.core-test
+  (:require [clojure.test :refer :all]
+            [flappy-bird.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
